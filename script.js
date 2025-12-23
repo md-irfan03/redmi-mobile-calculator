29248aefa96a3f7a3abdbca609a661ab6bab5129
MD IRFAN <168012868+md-irfan03@users.noreply.github.com>
2025-12-23 15:21:26 +0530

    Update script.js
    modifying calculate function for storing evaluations and adding functions for history and theme features.

--- script.js
+++ script.js
@@ -14,8 +14,41 @@ function deleteLast() {
 
 function calculate() {
   try {
-    display.value = eval(display.value);
+    const expression = display.value;
+    const result = eval(expression);
+
+    const li = document.createElement("li");
+    li.textContent = `${expression} = ${result}`;
+    li.onclick = () => display.value = result;
+    historyList.prepend(li);
+
+    display.value = result;
   } catch {
     display.value = "Error";
   }
 }
+function toggleHistory() {
+  historyPanel.classList.toggle("show");
+
+  const openIcon = document.getElementById("openIcon");
+  const closeIcon = document.getElementById("closeIcon");
+
+  if (historyPanel.classList.contains("show")) {
+    openIcon.style.display = "none";
+    closeIcon.style.display = "block";
+  } else {
+    openIcon.style.display = "block";
+    closeIcon.style.display = "none";
+  }
+}
+function clearHistory() {
+  document.getElementById("historyList").innerHTML = "";
+}
+const toggle = document.getElementById("themeSwitch");
+
+toggle.addEventListener("change", () => {
+  document.body.classList.toggle("dark");
+});
+openIcon.addEventListener("mouseenter", () => {
+  openIcon.setAttribute("data-title", "History");
+});
